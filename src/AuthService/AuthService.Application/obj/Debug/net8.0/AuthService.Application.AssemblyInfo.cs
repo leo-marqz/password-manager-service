e52d849fcae75c5fe4a413f952ccf14dec851e05
Elmer Marquez <leomarqz2020@gmail.com>
2025-04-04 17:02:28 -0600

    Se agrego el .gitignore en los proyectos AuthService

--- src/AuthService/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
+++ src/AuthService/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7860a2030e94eb70c90f311477823403dbd857ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab8696bffecabaf453af197413ffe5edbaa309b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
